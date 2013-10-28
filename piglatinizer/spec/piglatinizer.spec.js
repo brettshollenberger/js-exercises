1d08efec538fd7cf6e7676372e8826d5e2a2954f
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-27 23:44:32 -0400

    Initial commit for piglatinizer

---
+++ piglatinizer/spec/piglatinizer.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('piglatinizer', function() {
+
+  var piglatinizer;
+  beforeEach(function() {
+    piglatinizer = require('../lib/piglatinizer');
+  });
+
+  describe('Your first test', function() {
+
+    it('does something meaningful', function() {
+      expect(piglatinizer.prop1).toEqual('prop1');
+    });
+
+  });
+
+});
